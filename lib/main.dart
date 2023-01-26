c06175d246bf247ec6908a03fcc36d6d939aaf77
ZARIFMAC <uzbzarif@gmail.com>
2023-01-26 15:23:22 +0500

    get data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,17 +22,31 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
             title: const Text('Store a Data'),
           ),
-          body: TextField(
-            controller: _controller,
-            decoration: const InputDecoration(
-              hintText: 'Enter a some text',
-            ),
+          body: Column(
+            children: [
+              TextField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                  hintText: 'Enter a some text',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    final prefs = await SharedPreferences.getInstance();
+                    final counter = prefs.getInt('counter') ?? 0;
+
+                    print(counter.runtimeType);
+                  },
+                  child: Text('Get Data')),
+            ],
           ),
           floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                _controller.text = 'Hello World';
-              });
+            onPressed: () async {
+              final prefs = await SharedPreferences.getInstance();
+              await prefs.setInt('counter', int.parse(_controller.text));
             },
             child: const Icon(Icons.add),
           )),
